f8689e9fe7852132643529bcd7e63ed9c24feb85
Nelson Chimdiadi Chigozie Joel <sayhitonelson@gmail.com>
2025-05-13 10:48:08 +0100

    initialise site

---
+++ assessment/src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center bg-[#343a40] text-gray-300 p-4">
+      <div>
+        <h1>
+          <span>Nelson</span> Chimdiadi
+        </h1>
+      </div>
+      <div>
+        <ul>
+          <li>About Me </li>
+          <li>Skills</li>
+          <li>Hobbies & Interests</li>
+          <li>Passions</li>
+          <li>Academic Background</li>
+          <li>Work Experience</li>
+          <li>Certifications </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
